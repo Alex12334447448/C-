9a5d7f7a8fbe7fa4e28a0f0a10fa0d11040fe382
Alex12334447448 <147165270+Alex12334447448@users.noreply.github.com>
2023-11-08 01:18:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+static void Main()
+{
+Console.Write("Введите первое число: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Первое число: {num1:F2}");
+Console.WriteLine($"Второе число: {num2:F2}");
+Console.WriteLine($"Третье число: {num3:F2}");
+}
+}
